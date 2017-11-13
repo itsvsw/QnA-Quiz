99a680995925af39ccf4eb9bc5eab94adf6f43d0
itsvsw <31395604+itsvsw@users.noreply.github.com>
2017-11-12 19:02:07 -0500

    Add files via upload
    New and improved with question shuffling kung fu action... No answer/input (yet)

---
+++ openqa.py
@@ -0,0 +1,41 @@
+#openqa --- used to open a filehandle, read and delimit "questions and answers"
+#openqa --- based off '-' started 9NOV17 0730
+import random # random is used when determining what question should be asked
+print("Attempting to open file 'qatest.txt' in curdir...\n")
+inFile = open("qatest.txt",'r')
+q = []
+a = []
+for l in inFile:
+    i = l.find('-') #seperate questions and answers based of offset of '-' which divides Q from A
+    if i is -1:
+        continue #if line doesn't contain '-', then it must not be a question, because there is no answer. Adding formatting markers in future iterations.
+    q.append(l[0:i]) #from beginning up to, but not including the loc of '-'
+    a.append(l[i+1:-1]) #skips the '-' with i+1, to end of line
+if len(q) == len(a): #questions should equal answers, need error handling in case of not equal. Future debugging
+    print("Number of questions EQUALS number of answers. Stripping both\n")
+    x=0;
+    while x < len(q):
+        q[x] = q[x].strip()
+        a[x] = a[x].strip()
+        x += 1
+totalQ = len(q)
+reply = input(str(len(q)) + " questions found \nWould you like to proceed with quiz? (Y/N)")
+if reply[0].lower() == 'y':
+    print("YES")
+else:
+    print("NO")
+
+def startQ():
+    qAsked = 0
+    while True:
+        qNum = qOrder[0]
+        print("Question #" + str(qAsked+1) + ":" + q[qNum] + "\n")
+        qAsked += 1
+        qOrder.pop(0)
+        if qAsked == totalQ: break
+        
+
+qOrder = list(range(totalQ))
+random.shuffle(qOrder)
+print(qOrder)
+startQ()
