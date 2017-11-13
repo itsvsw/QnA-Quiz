54c8389421c4082c3708647d945f6c11b7ac6333
itsvsw <31395604+itsvsw@users.noreply.github.com>
2017-11-12 21:21:26 -0500

    Update openqa.py
    Added simple stats, also user input for answers.

--- openqa.py
+++ openqa.py
@@ -1,7 +1,7 @@
 #openqa --- used to open a filehandle, read and delimit "questions and answers"
 #openqa --- based off '-' started 9NOV17 0730
 import random # random is used when determining what question should be asked
-print("Attempting to open file 'qatest.txt' in curdir...\n")
+print("Attempting to open file 'qatest.txt' in curdir...")
 inFile = open("qatest.txt",'r')
 q = []
 a = []
@@ -12,7 +12,7 @@ for l in inFile:
     q.append(l[0:i]) #from beginning up to, but not including the loc of '-'
     a.append(l[i+1:-1]) #skips the '-' with i+1, to end of line
 if len(q) == len(a): #questions should equal answers, need error handling in case of not equal. Future debugging
-    print("Number of questions EQUALS number of answers. Stripping both\n")
+    print("Number of questions EQUALS number of answers. Stripping both")
     x=0;
     while x < len(q):
         q[x] = q[x].strip()
@@ -25,21 +25,25 @@ if reply[0].lower() == 'y':
 else:
     print("NO")
 def startQ():
-    qAsked = 0
-    while True:
-        qNum = qOrder[0]
-        qAsked += 1
-        print("Question #", str(qAsked), ":", q[qNum])        
-        qOrder.pop(0)
+    qOrder = list(range(totalQ)) #Makes a list of all numbers from 0 to the total number of Qs
+    random.shuffle(qOrder)#Shuffles that list
+    qAsked = 0 #Simple counter to keep track of what number the user is on
+    numCor = 0 #Amount of answers that are correct
+    while True:#To infinity and beyond
+        qNum = qOrder[0] 
+        qAsked += 1 #Add one before printing, test after asking questions, seems to work
+        print("Question #", str(qAsked), ": ", q[qNum], sep='')        
+        qOrder.pop(0) #Delete first index, that question has been asked.
         userAnswer = input()
         def checkA():
             if userAnswer.lower() == a[qNum].lower():
                 print("CORRECT")
-        checkA()
+                return True
+            else:
+                print("INCORRECT! The correct answer is...", a[qNum])
+        if checkA() is True: numCor += 1
         if qAsked == totalQ: break
-        
+    print(numCor, " out of ", totalQ," | ", round(numCor/totalQ*100,2),  "%", sep='')
+        #Once the number of question asked equals the total number of question        
 
-qOrder = list(range(totalQ))
-random.shuffle(qOrder)
-print(qOrder)
 startQ()
